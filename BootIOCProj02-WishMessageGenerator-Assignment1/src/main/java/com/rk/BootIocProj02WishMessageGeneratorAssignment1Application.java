02f3c8319eb392fa93adb60009ca231db7e99990
Raushan852 <raushan852125@gmail.com>
2025-08-02 00:47:10 +0530

    Second project completed

---
+++ BootIOCProj02-WishMessageGenerator-Assignment1/src/main/java/com/rk/BootIocProj02WishMessageGeneratorAssignment1Application.java
@@ -0,0 +1,35 @@
+package com.rk;
+
+import java.time.LocalTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import com.rk.sbeans.WishMessageGenerator;
+
+@SpringBootApplication
+public class BootIocProj02WishMessageGeneratorAssignment1Application {
+
+	@Bean("lt")
+	public LocalTime currentTime()
+	{
+		return LocalTime.now();
+	}
+	
+	public static void main(String[] args) {
+		try(ConfigurableApplicationContext ctx= SpringApplication.run(BootIocProj02WishMessageGeneratorAssignment1Application.class, args);)
+		{
+			WishMessageGenerator generator= ctx.getBean("wmg", WishMessageGenerator.class);
+			String showMessage = generator.showMessage();
+			System.out.println(showMessage);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+
+}
